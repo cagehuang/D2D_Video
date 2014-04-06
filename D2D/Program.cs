51849dcfa4440efb0e5faf736685cd24060ccb10
cagehuang <jackhuan.tw@yahoo.com.tw>
2014-04-06 17:03:13 +0800

    基地台&UE初步部屬完成 (包含顯示)

--- D2D/Program.cs
+++ D2D/Program.cs
@@ -14,7 +14,7 @@ namespace D2D
         {
             //**設定執行環境
 
-            Console.SetWindowSize(80, 51);
+            Console.SetWindowSize(80, 46);
 
             System.Diagnostics.Stopwatch stoptime = new System.Diagnostics.Stopwatch();////程式計時，引用stopwatch物件
             stoptime.Reset();//碼表歸零
@@ -24,14 +24,35 @@ namespace D2D
             //String result;
 
             int RUNTIME = 1 ; //程式執行次數
-            int UE_NUMBER = 10; //UE的個數
+            int UE_NUMBER = 3; //UE的個數
             int VIDEO_NUMBER = 3; //影片的個數
+
+            Show SHOW = new Show();
 
 
             //**程式執行次數
-            for (int runtime; runtime <= RUNTIME; runtime++) { 
+            for (int runtime=0; runtime <= RUNTIME; runtime++) { 
+                
                 //**部屬環境 (撒點、計算MCS、Conflict Graph...)
+                
+                //***部屬eNodeB & UE
+                UE[] UEs = new UE[UE_NUMBER + 1];//UE_Number +1 是因為要把eNodeB也算進去
 
+                //部屬eNodeB
+                UEs[0] = new UE();
+                UEs[0].UID = 0;
+                UEs[0].Set_eNodeB();
+
+                //部屬UEs
+                for (int UE_Number_Count = 1; UE_Number_Count < UE_NUMBER + 1; UE_Number_Count++){ //UE_NUMBER + 1是因為要算進eNodeB
+                    UEs[UE_Number_Count] = new UE(UE_Number_Count, UEs);
+                    UEs[UE_Number_Count].UID = UE_Number_Count;
+                }
+
+                SHOW.eNodeB_State(UEs);
+                SHOW.UEs_State(UE_NUMBER, UEs);
+                Console.ReadLine();
+                
 
                 //**沒有設計的排法
 
--- D2D/UE.cs
+++ D2D/UE.cs
@@ -13,7 +13,12 @@ namespace D2D
         public int X_AXIS = -1;         //X軸座標
         public int Y_AXIS = -1;         //Y軸座標
         public int MCS = -1;            //速率 (bits / slot)
-        public double eNodeB_Radius;    //eNodeB的覆蓋半徑
+        public double eNodeB_Radius = -1;    //eNodeB的覆蓋半徑
+
+        public UE()
+        {
+
+        }
 
         public UE(int UE_Number, UE[] UEs) {
             
@@ -26,11 +31,12 @@ namespace D2D
 
                 tmp_eNodeB_distance = Math.Sqrt(Math.Pow((X_AXIS - UEs[0].X_AXIS), 2) + Math.Pow((Y_AXIS - UEs[0].Y_AXIS), 2));
             } while (UEs[0].eNodeB_Radius < tmp_eNodeB_distance);
-
         }
 
         public void Set_eNodeB() {      //設定eNodeB   
             this.eNodeB_Radius = Convert.ToInt16(MCS_Calculate.Radius(0, 6)); //eNodeB的半徑，單位m;
+            this.X_AXIS = Convert.ToInt16(this.eNodeB_Radius);
+            this.Y_AXIS = Convert.ToInt16(this.eNodeB_Radius);
         }
 
     }

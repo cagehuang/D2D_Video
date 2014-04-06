2e55d43306035435332b5d5bd5b84ebf743f3ccd
cagehuang <jackhuan.tw@yahoo.com.tw>
2014-04-06 17:18:58 +0800

    嘗試合併Branch

---
+++ D2D/Show.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D2D
+{
+    class Show
+    {
+        public void eNodeB_State(UE[] UES)
+        {
+            Console.WriteLine("eNodeB： X_AXIS = " + UES[0].X_AXIS +
+                    ", Y_AXIS = " + UES[0].Y_AXIS + ", Radius = " + UES[0].eNodeB_Radius);
+            Console.WriteLine();
+        }
+        
+        public void UEs_State(int UE_NUMBER, UE[] UES)
+        {
+            for (int UE_Number = 1; UE_Number <= UE_NUMBER; UE_Number++) 
+            {
+                Console.WriteLine("UE " + UES[UE_Number].UID + "  ：" + "X_AXIS = " + UES[UE_Number].X_AXIS + 
+                    ", Y_AXIS = " + UES[UE_Number].Y_AXIS);
+            }
+            Console.WriteLine();
+        }
+    }
+}
